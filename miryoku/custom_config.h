68561ae8c43ddc8676dd8414da5f7ec9dc4a9a54
Puddingkuchen <81821968+Puddingkuchen@users.noreply.github.com>
2024-10-21 11:07:57 +0200

    Update custom_config.h
    swap z and y
    add backspace to kyria

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,28 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
+
+#define XXX &none
+
+ #if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
+  #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+  )\
+   XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
+   &kp BSPC K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
+   XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+                  XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+  #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
+  #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
+ #endif
+#endif
+
+#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Z,             &umlaute,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
