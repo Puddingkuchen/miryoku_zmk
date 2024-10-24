c0b4940325c398e8f39ff13828e4724b775fb0a6
Puddingkuchen <81821968+Puddingkuchen@users.noreply.github.com>
2024-10-24 09:34:03 +0200

    Update custom_config.h
    use outer pinkies on totem for layers.
    change symbols for german layout.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,7 @@
 
 #if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
 
-#define XXX &none
+ #define XXX &none
 
  #if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
   #define MIRYOKU_LAYOUTMAPPING_KYRIA(\
@@ -21,8 +21,40 @@
  #endif
 #endif
 
+#if !defined (MIRYOKU_LAYOUTMAPPING_TOTEM)
+
+ #define XXX &none
+
+ #define MIRYOKU_LAYOUTMAPPING_TOTEM( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+ ) \
+     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K36  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K33\
+               K32  K33  K34       K35  K36  K37
+
+ #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 33 34
+ #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 35 36
+
+#endif
+
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Z,             &umlaute,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_NUM \
+&kp SQT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp MINUS,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
