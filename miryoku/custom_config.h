b4279a92e273e83be2f68a9b361665046f42f5cb
Puddingkuchen <81821968+Puddingkuchen@users.noreply.github.com>
2025-12-12 15:56:48 +0100

    remove mapping for now

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,25 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
-
-#define XXX &none
-
-#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
-     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
-)\
-XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
-XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
-XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
-               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
-
-#endif
-
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Z,             &umlaute,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
