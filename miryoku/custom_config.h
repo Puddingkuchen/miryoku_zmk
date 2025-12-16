6f0497218c742ce86a5b812af1afa0f611154480
Puddingkuchen <github@puddingspeak.com>
2025-12-16 16:09:03 +0100

    Fix incomplete copy in layers Readd mapping

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,25 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#if !defined (MIRYOKU_LAYOUTMAPPING_KYRIA)
+
+#define XXX &none
+
+#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+)\
+K37  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  K32  \
+K36  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  K33  \
+K35  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  K34  \
+               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
+
+#endif
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Z,             &umlaute,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
@@ -17,5 +36,6 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,   
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
 
